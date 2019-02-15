569eafff568e30a35015e5138132aa02b611f959
Simon Davis <simon_freelance@hotmail.com>
2019-02-15 11:59:27 -0500

    GET routes for id, year, and brand implemented

--- src/main/java/com/lambdaschool/cars/CarController.java
+++ src/main/java/com/lambdaschool/cars/CarController.java
@@ -2,12 +2,10 @@ package com.lambdaschool.cars;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -24,6 +22,32 @@ public class CarController {
     public List<Car> all() {
         return carRepo.findAll();
     }
+    @GetMapping("/cars/id/{id}")
+    public Car getCarById(@PathVariable Long id) {
+        return carRepo.findById(id)
+                .orElseThrow(() -> new CarNotFoundException(id));
+    }
+
+    @GetMapping("/cars/year/{year}")
+    public List<Car> getCarsByYearModel(@PathVariable int year) {
+        return carRepo.findAll()
+                .stream()
+                .filter(car -> car.getYear() == year)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/cars/brand/{brand}")
+    public List<Car> getCarsByBrand(@PathVariable String brand) {
+
+        CarLog message = new CarLog("Search for " + brand);
+        log.info("Search for " + brand);
+
+        return carRepo.findAll()
+                .stream()
+                .filter(car -> car.getBrand().equalsIgnoreCase(brand))
+                .collect(Collectors.toList());
+    }
+
     @PostMapping("/cars/upload")
     public List<Car> uploadCars(@RequestBody List<Car> newCars) {
         return carRepo.saveAll(newCars);
